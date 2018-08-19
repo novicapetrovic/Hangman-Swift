a40187ea2a0e1d6a374bce9e2777021e9d72fa59
Novica Petrovic <novicapetrovic97@gmail.com>
2018-08-19 14:56:39 +0100

    Initial Commit

---
+++ Hangman/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hangman
+//
+//  Created by Novica Petrovic on 19/08/2018.
+//  Copyright © 2018 Novica Petrovic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
