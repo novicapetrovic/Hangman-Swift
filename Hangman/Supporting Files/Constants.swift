7b6cc99351fb35a57dbd012edb2382db9465162a
Shiva Skanthan <sshiva93@hotmail.com>
2018-09-17 11:57:59 +0800

    Reorganise code based types (Variables and Constants, IBOutlet, IBAction, Function..) Change soundArray from var to let Rename restartButton function to restartGame Create a Constants file to store all String objects for sound and images Moved all code related to starting a game in viewDidLoad to a new startGame function. All the code within the function is still the same

---
+++ Hangman/Supporting Files/Constants.swift
@@ -0,0 +1,31 @@
+//
+//  Constants.swift
+//  Hangman
+//
+//  Created by Shiva Skanthan on 17/09/2018.
+//  Copyright © 2018 Novica Petrovic. All rights reserved.
+//
+
+import Foundation
+
+//MARK:- Images
+
+public let hangman0 = "hangman0"
+public let hangman1 = "hangman1"
+public let hangman2 = "hangman2"
+public let hangman3 = "hangman3"
+public let hangman4 = "hangman4"
+public let hangman5 = "hangman5"
+public let hangman6 = "hangman6"
+
+//MARK:- Sounds
+public let sound0 = "Creepy_Percussion_8"
+public let sound1 = "Creepy-Roll-Over-2"
+public let sound2 = "Jigsaw Laugh 2017"
+public let sound3 = "Correct Answer Sound Effect"
+
+//MARK:- Background Music
+public let background_Music = "The-Island-of-Dr-Sinister"
+
+//MARK:- Extension
+public let mp3 = "mp3"
